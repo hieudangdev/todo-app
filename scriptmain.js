c72f0cee870b6a19a35061d47d7765d51a414d0f
codeittolearn <dangthanhhieu230@gmail.com>
2021-11-27 15:07:26 +0700

    aa

---
+++ scriptmain.js
@@ -0,0 +1,7 @@
+// getting all required elements
+const inputBox = document.querySelector(".inputField input");
+const addBtn = document.querySelector(".inputField button");
+const todoList = document.querySelector(".todoList");
+const deleteAllBtn = document.querySelector(".footer button");
+
+
